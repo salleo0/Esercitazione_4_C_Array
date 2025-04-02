2d7284ee5544ee9aa84cf196afffbb0d322d042b
Leonardo Sala <s310598@studenti.polito.it>
2025-04-02 10:18:36 +0200

    file sorgente aggiunti

---
+++ Exercise1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Utils.hpp" // g++ requires: "src/Utils.hpp"
+// #include "ClassObj.hpp" // error if pragma once is not used in ClassObj
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+
