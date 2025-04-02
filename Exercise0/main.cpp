56ad998f86a4856a4009f2b914dc2ef3bff4b41a
Leonardo Sala <s310598@studenti.polito.it>
2025-04-02 10:27:24 +0200

    aggiornamento file main.cpp in Exercise0

--- Exercise0/main.cpp
+++ Exercise0/main.cpp
@@ -1,5 +1,31 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
+	int a = 2;
+	int &b = a;
+	const int *p = nullptr;
+	
+	cout << *p << endl;
+	
+	a++;
+	// b++;
+	// (*p)++;
+	
+	int c = 4;
+	p = &c;
+	
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	cout << "*p: " << *p << endl;
+	
+	cout << "p: " << p << endl;
+	cout << "&a: " << &a << endl;
+	cout << "&b: " << &b << endl;
+	cout << "&p: " << &p << endl;
+	
+	
     return 0;
 }
 
