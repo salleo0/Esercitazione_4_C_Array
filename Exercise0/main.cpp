ae6376c86d88af66441acdf53441f60f2b8f27a2
Leonardo Sala <s310598@studenti.polito.it>
2025-04-09 19:00:30 +0200

    Exercise0: finito

--- Exercise0/main.cpp
+++ Exercise0/main.cpp
@@ -1,50 +1,93 @@
 #include <iostream>
 using namespace std;
 
+int foo(int& a)
+{
+	a++;
+	return a;
+}
+
+int* fooPtr(int* &ptr)
+{
+	(*ptr)++;
+	
+	int b = 2;
+	ptr = &b;	// Attention: b will be deleted after the return statement
+	
+	return ptr;
+}
+
 int main()
 {
-	/*
 	int a = 2;
-	int &b = a;
-	const int *p = nullptr;
+	int b = 3;
+	int &c = b;
+	int *ptr1 = &a;	// & is the address-of operator
+	int * &ptr = ptr1; // ptr is a reference for ptr1
+	// int &* ptr;		error: references are not objects, they do not have addresses. Hence
+	//						   we may not define a pointer to a reference
+	cout << ptr1 << endl;
+	cout << ptr << endl;
 	
-	cout << *p << endl;
+	ptr = &c;
 	
-	a++;
-	// b++;
-	// (*p)++;
-	
-	int c = 4;
-	p = &c;
-	
-	cout << "a: " << a << endl;
-	cout << "b: " << b << endl;
-	cout << "*p: " << *p << endl;
-	
-	cout << "p: " << p << endl;
-	cout << "&a: " << &a << endl;
+	cout << "ptr1: " << ptr1 << endl;
+	cout << "ptr: " << ptr << endl;
+	cout <<"&c: " << &c << endl;	// the reference is not an object
 	cout << "&b: " << &b << endl;
-	cout << "&p: " << &p << endl;
-	*/
 	
-	// ARRAY 
-	int i = 3;
-	const unsigned int j = 3;
-	int arr[j] = {1,2,3};
-	for(unsigned int i = 0; i < 3; i++)
-		cout << "arr[" << i << "]: " << arr[i] << endl;
+	cout << "&ptr: " << &ptr << endl;	// a pointer is an object itself
 	
-	int *ptr1 = new int[i];
+	// type of object and pointer must match. The following code produces an error:
+	// double a = 2.0;
+	//int *ptr1 = &a;
 	
-	int *ptr2 = &arr[0];
-	cout << "*ptr2: " << *ptr2 << endl;
-	cout << "arr[0]: " << arr[0] << endl;
-	cout << "*arr: " << *arr << endl;
-	cout << "*(ptr2 + 1): " << *(ptr2 + 1) << endl;
-
-	delete [] ptr1;
+	// * is the dereference operator
+	cout << "*ptr: " << *ptr << endl;
+	*ptr = 4;
+	cout << "b: " << b << endl;
 	
+	// it is an error to try to derefence a pointer that does not point to an object
+	int *nptr = nullptr;	// nullptr is a literal
 	
+	// cout << *nptr << endl; 		error: the program will crash
+	
+	/// pointers and const
+	const double pi = 3.14;
+	// double *ptr = &pi; //error
+	const double *cptr = &pi;
+	// *cptr = 42; error
+	double s = 1.0;
+	cptr = &s; 		// it works
+	// *cptr = 32;	// error: cannot change s through cptr
+	const double *const pip = &pi;	// const pointer to const double
+	
+	int &ref = a;
+	cout << "a: " << a << endl;
+	cout << foo(a) << endl;
+	cout << "a: " << a << endl;
+	cout << "ptr: " << ptr << endl;
+	
+	// ARRAY (static)
+	int i = 2;
+	const unsigned int j = i;
+	const unsigned int m = 5;
+	const size_t n = 2;
+	int arr[m] = {1,2};		// use i or j to dimension the array is an error
+	int arr2[n];			// = arr; // no copy
+	// arr2 = arr;			// no assignment
+	
+	int *aptr = &arr[0];
+	cout << "aptr[0]: " << aptr[0] << endl;
+	// int *jptr = aptr++;
+	int *jptr = arr+1; 		// arr++ is not allowed
+	cout << "*jptr: " << *jptr << endl;
+	
+	// ARRAY (dynamic)
+	int k = 2;
+	int *ptr3 = new int[k]{1,2};
+	
+	delete [] ptr3;
 	
     return 0;
 }
